ddf6aec4c940b177fd90d12874a4af12a89129bf
therealjameswilson <30484292+therealjameswilson@users.noreply.github.com>
2026-05-29 06:58:54 -0400

    Create FRUS assist page

---
+++ data/volume.js
@@ -0,0 +1,12 @@
+window.FRUS_VOLUME = {
+  "index": 3,
+  "doc_id": "frus1956-60IntelCom",
+  "title": "1956\u20131960, The Intelligence Community",
+  "official_url": "https://history.state.gov/historicaldocuments/frus1956-60IntelCom",
+  "repo_slug": "frus-1956-60intelcom-assist",
+  "title_ascii": "1956-1960, The Intelligence Community",
+  "pages_url": "https://therealjameswilson.github.io/frus-1956-60intelcom-assist/",
+  "repo_url": "https://github.com/therealjameswilson/frus-1956-60intelcom-assist",
+  "nara_scout_url": "https://therealjameswilson.github.io/nara-scout/#q=The+OR+Intelligence+OR+Community&from=1956&to=1960&sort=relevance&perColl=25&perPage=50",
+  "companion": null
+};
